197e86645db6ce587d30d86955e41c47cf550a8d
Valerie Barela <vbarela1@gmail.com>
2017-05-09 12:35:39 -0600

    adds devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "home#index"
+
+    devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
+
+    namespace :api do
+      get 'user_id', to: 'users#user_id'
+    end
+
+    get '*unmatched_route', to: 'home#index'
 end
